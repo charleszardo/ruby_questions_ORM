d8146bb8ed8fe553adb3cdbeda98909f517e6655
Dave Charnuska <davecharnuska@gmail.com>
2016-08-05 18:12:29 -0400

    add QuestionLike::likers_for_question_id(question_id), QuestionLike::num_likes_for_question_id(question_id)

--- questions.rb
+++ questions.rb
@@ -332,9 +332,43 @@ class QuestionLike
     QuestionLike.new(question_like.first)
   end
 
+  def self.likers_for_question_id(question_id)
+    likers = QuestionDBConnection.instance.execute(<<-SQL, question_id)
+      SELECT
+        users.*
+      FROM
+        users
+      JOIN
+        question_likes
+      ON
+        users.id = question_likes.user_id
+      WHERE
+        question_likes.question_id = ?
+    SQL
+
+    return nil unless likers.length > 0
+
+    likers.map { |liker| User.new(liker) }
+  end
+
+  def self.num_likes_for_question_id(question_id)
+    likes = QuestionDBConnection.instance.execute(<<-SQL, question_id)
+      SELECT
+        COUNT(id) AS num
+      FROM
+        question_likes
+      WHERE
+        question_id = ?
+    SQL
+
+    return likes.first["num"]
+  end
+
   def initialize(options)
     @id = options['id']
     @question_id = options['question_id']
     @user_id = options['user_id']
   end
 end
+
+p QuestionLike.num_likes_for_question_id(3)
